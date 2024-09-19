bcf871c96e3e5eff4389d568f9557391cc4c983f
Alejandro Brugarolas <abrugaro@redhat.com>
2024-09-19 12:41:28 +0200

    first script version
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

---
+++ popultate-konveyor.sh
@@ -0,0 +1,262 @@
+#!/bin/bash
+
+baseName="Test"
+count=10
+
+declare -A app1=(
+  [name]="Tackle-testapp public"
+  [url]="https://github.com/konveyor/tackle-testapp-public.git"
+  [oldBranch]="feature/test-java9-removed-packagek"
+  [newBranch]="main"
+  [target]="cloud-readiness"
+)
+
+declare -A app2=(
+  [name]="Day Trader"
+  [url]="https://github.com/WASdev/sample.daytrader7.git"
+  [oldBranch]="session"
+  [newBranch]="master"
+  [targets]="quarkus"
+)
+
+apps=("app1" "app2")
+declare -A createdApps
+
+pid=$$
+self=$(basename $0)
+tmp=/tmp/${self}-${pid}
+
+usage() {
+  echo "Usage: ${self} <required> <options>"
+  echo "Auth has to be disabled in order to execute this script"
+  echo "-h help"
+  echo "Required:"
+  echo "  -u <URL>/hub"
+  echo "Options:"
+  echo "  -b base name (Test)"
+  echo "  -n count  (10)"
+  echo "  -o output"
+}
+
+while getopts "u:b:n:o:h" arg; do
+  case $arg in
+  u)
+    host=$OPTARG
+    ;;
+  b)
+    baseName=$OPTARG
+    ;;
+  n)
+    count=$OPTARG
+    ;;
+  o)
+    output=$OPTARG
+    ;;
+  h)
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z "${host}" ]; then
+  echo "-u required."
+  usage
+  exit 0
+fi
+
+print() {
+  if [ -n "${output}" ]; then
+    echo -e "$@" >>${output}
+  else
+    echo -e "$@"
+  fi
+}
+
+echo
+echo "Host:   ${host}"
+echo "Name:   ${baseName}"
+echo "Count:  ${count}"
+echo
+answer="y"
+read -p "Continue[Y,n]: " answer
+if [ "$answer" != "y" ]; then
+  exit 0
+fi
+
+createAnalysis() {
+  appId=$1
+  appName=$2
+  appTarget=$3
+  d="
+{
+    \"name\": \"taskgroup.analyzer\",
+    \"kind\": \"analyzer\",
+    \"state\": \"Created\",
+    \"priority\": 10,
+    \"data\": {
+        \"tagger\": {
+            \"enabled\": true
+        },
+        \"mode\": {
+            \"binary\": false,
+            \"withDeps\": true
+        },
+        \"rules\": {
+            \"labels\": {
+                \"included\": [
+                    \"konveyor.io/target=${appTarget}\"
+                ]
+            }
+        }
+    },
+    \"tasks\": [
+        {
+            \"name\": \"${appName}.${appId}.windup\",
+            \"application\": {
+                \"id\": ${appId},
+                \"name\": \"${appName}\"
+            }
+        }
+    ]
+}"
+
+  # Create taskgroup
+  code=$(curl -kSs -o ${tmp} -w "%{http_code}" -X POST ${host}/taskgroups -H 'Content-Type:application/json' -H 'Accept: application/json' -d "${d}")
+  if [ ! $? -eq 0 ]; then
+    exit $?
+  fi
+  case ${code} in
+  201)
+    taskGroupId=$(jq .id ${tmp})
+    print "TaskGroup ${taskGroupId} CREATED Taskgroup for application: ${appName} id=${appId}"
+    ;;
+  *)
+    print "Create task for: appId=${appId} - FAILED: ${code}."
+    cat ${tmp}c
+    exit 1
+    ;;
+  esac
+
+  # Start analysis
+  code=$(curl -kSs -o ${tmp} -w "%{http_code}" -X PUT ${host}/taskgroups/${taskGroupId}/submit -H 'Content-Type:application/json' -d "${d}")
+  if [ ! $? -eq 0 ]; then
+    exit $?
+  fi
+  case ${code} in
+  204)
+    id=$(jq .id ${tmp})
+    print "Analysis ${id} STARTED for application: ${appName} id=${appId}"
+    #updateBranch ${appId} ${appName} "https://github.com/konveyor/tackle-testapp-public.git" "main"
+    ;;
+  *)
+    print "Start analysis for: appId=${appId} - FAILED: ${code}."
+    cat ${tmp}
+    exit 1
+    ;;
+  esac
+}
+
+updateBranch() {
+  appId=$1
+  appName=$2
+  repositoryUrl=$3
+  newBranch=$4
+  d="
+---
+name: ${appName}
+description: ${appName} Test application.
+repository:
+  kind: git
+  branch: ${newBranch}
+  url: ${repositoryUrl}
+tags:
+"
+  code=$(curl -kSs -o ${tmp} -w "%{http_code}" -X PUT ${host}/applications/${appId} -H 'Content-Type:application/x-yaml' -d "${d}")
+  if [ ! $? -eq 0 ]; then
+    exit $?
+  fi
+  case ${code} in
+  204)
+    print "Application ${appName} id=${appId} - UPDATED"
+    ;;
+  *)
+    print "Update application ${appName} - FAILED: ${code}."
+    cat ${tmp}
+    exit 1
+    ;;
+  esac
+}
+
+waitForAnalyses() {
+  while true; do
+    code=$(curl -kSs -o ${tmp} -w "%{http_code}" ${host}/tasks/report/queue)
+    total=$(jq .total ${tmp})
+
+    if [ "$total" -eq 0 ]; then
+      echo "All analyses are finished"
+      break
+    fi
+
+    echo "Some analyses are still running, waiting 30 secs..."
+    sleep 30
+  done
+}
+
+createApplications() {
+  for i in $(seq 1 ${count}); do
+
+    randomApp=${apps[$RANDOM % ${#apps[@]}]}
+
+    appName="$(eval echo \${${randomApp}[name]})-$i"
+    appUrl=$(eval echo \${${randomApp}[url]})
+    appBranch=$(eval echo \${${randomApp}[oldBranch]})
+    appTarget=$(eval echo \${${randomApp}[target]})
+
+    d="
+---
+name: ${appName}
+description: ${appName} Test application.
+repository:
+  kind: git
+  branch: ${appBranch}
+  url: ${appUrl}
+tags:
+- id: 16
+"
+    code=$(curl -kSs -o ${tmp} -w "%{http_code}" -X POST ${host}/applications -H 'Content-Type:application/x-yaml' -d "${d}")
+    if [ ! $? -eq 0 ]; then
+      exit $?
+    fi
+    case ${code} in
+    201)
+      id=$(jq .id ${tmp})
+      print "Application ${appName} id=${id} - CREATED"
+
+      createdApps["${id}"]="${randomApp}"
+
+      createAnalysis ${id} "${appName}" "${appTarget}"
+      ;;
+    *)
+      print "Create application ${appName} - FAILED: ${code}."
+      cat ${tmp}
+      exit 1
+      ;;
+    esac
+  done
+
+  waitForAnalyses
+
+  for appId in "${!createdApps[@]}"; do
+    appKey="${createdApps[$appId]}"
+
+    appName=$(eval echo \${${appKey}[name]})
+    appUrl=$(eval echo \${${appKey}[url]})
+    appTarget=$(eval echo \${${appKey}[target]})
+    appNewBranch=$(eval echo \${${appKey}[newBranch]})
+
+    updateBranch $appId "${appName}" "${appUrl}" "${appNewBranch}"
+  done
+}
+
+createApplications
